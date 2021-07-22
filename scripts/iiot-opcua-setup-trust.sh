3d8080d6f40cbdff2da84c3b1adec8d4ff3fdde6
Deniz Etkar <denizetkar@gmail.com>
2021-07-22 19:41:29 +0100

    trust establisher script assumes the user is in 'docker' group

--- scripts/iiot-opcua-setup-trust.sh
+++ scripts/iiot-opcua-setup-trust.sh
@@ -13,9 +13,9 @@ fi
 
 rm -rf ./certificates/opcua ./certificates/iiot || exit 1
 sudo -u $real_user mkdir -p ./certificates/opcua && cd ./certificates/opcua || exit 1
-docker cp opc-ua-edge-server:/root/.config/node-opcua-default-nodejs/PKI/own/certs/. . || exit 1
-openssl x509 -inform PEM -outform DER -text -in certificate.pem -out opcua_cert.der || exit 1
-mv opcua_cert.der /mount/pki/trusted/certs || exit 1
+sudo -u $real_user docker cp opc-ua-edge-server:/root/.config/node-opcua-default-nodejs/PKI/own/certs/. . || exit 1
+sudo -u $real_user openssl x509 -inform PEM -outform DER -text -in certificate.pem -out opcua_cert.der || exit 1
+cp -f opcua_cert.der /mount/pki/trusted/certs || exit 1
 chown root:root /mount/pki/trusted/certs/opcua_cert.der || exit 1
 
 cd .. && sudo -u $real_user mkdir -p ./iiot && cd iiot || exit 1
@@ -25,4 +25,5 @@ for f in *.der; do
     sudo -u $real_user openssl x509 -inform DER -outform PEM -text -in "${f}" -out "${f}.pem" || exit 1
     sudo -u $real_user rm -f "${f}" || exit 1
 done
-docker cp . opc-ua-edge-server:/root/.config/node-opcua-default-nodejs/PKI/trusted/certs || exit 1
+sudo -u $real_user docker cp . opc-ua-edge-server:/root/.config/node-opcua-default-nodejs/PKI/trusted/certs || exit 1
+# sudo -u $real_user docker exec opc-ua-edge-server chown -R root:root /root/.config/node-opcua-default-nodejs/PKI/trusted/certs || exit 1
